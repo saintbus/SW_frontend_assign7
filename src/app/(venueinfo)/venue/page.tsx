7c8ae48f4ceb7e3e500a2cd933fb2ebeff433f4a
saintbus <trinbus9291@gmail.com>
2025-03-16 23:18:03 +0700

    test

---
+++ src/app/(venueinfo)/venue/page.tsx
@@ -0,0 +1,10 @@
+import CardPanel from "@/components/CardPanel";
+
+export default function Venue() {
+    return(
+        <main>
+            <h1 className="text-xl font-medium text-center mb-5 mt-5">Select Your Venue</h1>
+            <CardPanel/>
+        </main>
+    );
+}
